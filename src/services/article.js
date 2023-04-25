caea8b8345484507abef6ddb2e002d08d35d5717
Leandro Reis <developer.reis@gmail.com>
2023-04-25 06:40:16 +0100

    finished

--- src/services/article.js
+++ src/services/article.js
@@ -1,13 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const rapidapi_key = import.meta.env.VITE_RAPIDAPI_SUMMARIZER_KEY;
+
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: ''
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key',rapidapi_key);
+            headers.set('X-RapidAPI-Host','article-extractor-and-summarizer.p.rapidapi.com');
+            return headers;
+        }
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `test`
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
         })
     })
 });
+
+export const { useLazyGetSummaryQuery } = articleApi;
